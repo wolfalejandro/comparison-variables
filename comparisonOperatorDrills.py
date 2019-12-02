d3a42c942fc01e29817f2d3e17965f0f879f8ab2
wolfalejandro <57291114+wolfalejandro@users.noreply.github.com>
2019-12-01 23:48:55 -0600

    Add files via upload

---
+++ comparisonOperatorDrills.py
@@ -0,0 +1,153 @@
+'''
+For this assignment you should read the task, then below the task do what it asks you to do
+EXAMPLE TASK:
+'''
+#EX) Check if 4 is equal to 5 and store the answer in variable 
+a = (4==5)
+
+'''
+END OF EXAMPLE
+'''
+
+'''
+START HERE
+'''
+
+#1) Check if "dog" is equal to "cat" and store the answer in variable b
+b= "dog" == "cat"
+print(b)
+#2) Check if 1.24 is equal to 1.25 and store the answer in variable c
+c= 1.24 == 1.25
+print(c)
+#3) Check if True is equal to False and store the answer in variable d
+d= True == False
+print(d)
+#4) Check if [1,2,3] is equal to [1,2,3] and store the answer in variable e
+e= [1,2,3] == [1,2,3]
+print(e)
+#5) Check if 1 is less than or equal to 2 and store the answer in variable f
+f= 1 <= 2
+print(f)
+#6) Check if 1 is greater than or equal to 2 and store the answer in variable g
+g= 1 >= 2
+print(g)
+#7) Check if 1.66 is greater than or equal to 1.67 and store the answer in variable h
+h= 1.66 >= 1.67
+print(h)
+#8) Check if 1.66 is less than or equal to 1.67 and store the answer in variable i
+i= 1.66 <= 1.67
+print(i)
+#9) Check if 1 is less than 2 and store the answer in variable j
+j= 1 < 2
+print(j)
+#10) Check if 1 is greater than 2 and store the answer in variable k
+k= 1 > 2
+print(k)
+#11) Check if 11 divided by three 3 is less than 100 divided by 33 and store the answer
+#  ) in variable l
+l= 11/3 < 100/33
+print(l)
+#12) Check if 9 times 7 is greater than 8 times 8 and store the answer in variable m
+m= 9*7 > 8*8
+print(m)
+#13) Check if the square root of 3 is less than 1.15 squared and store the answer in
+#  ) variable n
+n =  1.73 < 1.15**2
+#14) Multiple 8 by 3 and store that in variable o, then divide 56 by 2 and store that
+#  ) in variable p, finally check if o is greater then p and store the answer in variable q.
+o= 8*3
+p= 56/2
+q= o > p
+#15) Take the square root of 96 and store that in variable r, then square 8.6 and store
+#  ) that in variable s, finally check if r is greater than or equal to s and store the
+#  ) answer in t
+r= 9.79
+s= 8.6**2
+t= r >= s
+'''
+For the next section, you will be reading comparison statements and determining their answer
+EXAMPLE:
+'''
+#EX) 4 == 5
+False
+
+'''
+END OF EXAMPLE
+'''
+
+'''
+START HERE
+'''
+
+#1) 4 == 5
+False
+
+#2) 5 == 5
+True
+#3) 3 == 3
+True
+#4) a = 2
+# ) a == 2
+True
+#5) a = 3
+# ) b = 2
+# ) a == b
+False
+#6) a = 4
+# ) b = 4
+# ) a == b
+True
+#7) a = 6
+# ) b = (12/2)
+# ) a == b
+True
+#8) a = (3/3)
+# ) b = (13/13)
+# ) a == b
+True
+#9) a = 3
+# ) b = math.sqrt(9)
+# ) a == b
+True
+#10) a = "word"
+#  ) b = "Word"
+#  ) a == b
+True
+#11) (10/5) == (12/6)
+True
+#12) (14/7) == (15/5)
+False
+#13) a = 10
+#  ) b = 5
+#  ) (a/b) == 2
+True
+#14) a = 10
+#  ) b = 5
+#  ) (a/b) == (12/6)
+True
+#15) a = 10
+#  ) b = 5
+#  ) c = 12
+#  ) d = 6
+#  ) (a/b) == (12/6)
+True
+#16) a = "something"
+#  ) b = "some thing"
+#  ) a == b
+False
+#17) a = "word"
+#  ) b = "word"
+#  ) a == b
+True
+#18) a = True
+#  ) a == True
+True
+#19) a = True
+#  ) b = False
+#  ) a == b
+False
+#20) a = True
+#  ) A = False
+#  ) b = False
+#  ) A == b
+True
---
+++ debugComparisonOperatorDrills.py
@@ -0,0 +1,93 @@
+'''
+For this assignment you should look at the code created below and find the error.
+For each task, there will be one error that you must find and correct.
+Sometimes there will be an explanation of the problem and/or tips that can help you
+complete the tasks.
+EXAMPLE TASK:
+'''
+#EX)
+#  )Broken:
+#  )5 = 5
+#  )Correct:
+5 == 5
+
+'''
+END EXAMPLE
+'''
+
+'''
+START HERE
+'''
+
+#1)
+# )Broken:
+# )4 = 4
+# )Correct:
+4 == 4
+#2)
+# )Broken:
+# )3 = 3
+# )Correct:
+3 == 3
+#3)
+# )Broken:
+# )5 = = 5
+# )Correct:
+5 == 5
+#4)
+# )Broken:
+# )4 = = 4
+# )Correct:
+4 == 4
+#5)
+# )Broken:
+# )a = 4
+# )a = 3
+# )Correct:
+a= 4
+b= 3
+#6)
+# )Broken:
+# )a = 4
+# )b = 3
+# )a = b
+# )Correct:
+a = 4
+b = 4
+a == b
+#7)
+# )Broken:
+# )a = 3
+# )a < 4
+# )PROBLEM: I want it to be true when a is less than OR equal to 4
+# )Correct:
+a = 3
+a <= 4
+#8)
+# )Broken:
+# )a = 3
+# )a > 4
+# )PROBLEM: I want it to be true when a is greater than OR equal to 4
+# )Correct:
+a = 4
+a >= 4
+#9)
+# )Broken:
+# )a = 3
+# )b = 3
+# )a < 3
+# )PROBLEM: I want it to be true when a is less than OR equal to b
+# )Correct:
+a = 3
+b = 3
+a <= b
+#10)
+# )Broken:
+# )a = 3
+# )b = 2
+# )a > b
+# )PROBLEM: I want it to be true when a is greater than OR equal to b
+# )Correct:
+a = 3
+b = 2 
+a >= b
